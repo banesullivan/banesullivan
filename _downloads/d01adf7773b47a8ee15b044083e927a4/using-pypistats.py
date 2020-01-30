00348ec82f826154ad29bb7dc2e6cf33bc37c1f3
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-30 04:15:11 +0000

    Update docs after building Travis build 26 of banesullivan/mywebsite
    The docs were built from the branch 'master' against the commit
    52bcdbf8a4394bed15b6cb2619b0d662805367bd.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/banesullivan/mywebsite/jobs/643694745.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy . --built-docs site/_build/html --key-path ./github_deploy_key_banesullivan_mywebsite.enc

---
+++ _downloads/d01adf7773b47a8ee15b044083e927a4/using-pypistats.py
@@ -0,0 +1,69 @@
+"""
+Using PyPIstats
+---------------
+
+Not too long ago, I made a `pull request <https://github.com/hugovk/pypistats/pull/74>`_
+to `PyPIstats <https://github.com/hugovk/pypistats>`_ to be able to make Pandas
+:class:`DataFrame` objects of the download stats for any given project on PyPI.
+It's a nifty little tool!
+
+This example is mostly so that I can remember how to use PyPIstats and to have
+a place where my favorite Python projects' stats are reported whenever my
+website is automatically rebuilt.
+
+Not much to see here unless you're interested in how many downloads my favorite
+projects are getting lately.
+
+"""
+
+# sphinx_gallery_thumbnail_number = 1
+import pypistats
+import matplotlib.pyplot as plt
+
+def fetch_and_plot(name):
+    """A little helper method to do it all and make it pretty."""
+    data = pypistats.overall(name, total=True, format="pandas")
+    data = data.groupby("category").get_group("without_mirrors").sort_values("date")
+
+    data.plot(x="date", y="downloads", figsize=(15, 5), marker='o')
+    plt.xticks(
+        rotation=45,
+        horizontalalignment='right',
+        fontweight='light',
+        fontsize='medium',
+    )
+    plt.title("Downloads for {}".format(name))
+    plt.tight_layout()
+    return plt.show()
+
+###############################################################################
+
+fetch_and_plot("pyvista")
+
+###############################################################################
+
+fetch_and_plot("scooby")
+
+###############################################################################
+
+fetch_and_plot("SimPEG")
+
+###############################################################################
+
+fetch_and_plot("PVGeo")
+
+###############################################################################
+
+fetch_and_plot("itkwidgets")
+
+###############################################################################
+
+fetch_and_plot("gempy")
+
+###############################################################################
+
+fetch_and_plot("omf")
+
+###############################################################################
+
+fetch_and_plot("omfvista")
