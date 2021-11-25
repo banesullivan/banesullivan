8fedd0e08abb85b11270cd1f410be4511d1fed90
banesullivan <banesullivan@gmail.com>
2021-11-25 00:02:47 +0000

    Deploying to gh-pages from @ banesullivan/mywebsite@042f4c2a23cb84887426f4fa636fd3c657bac3f1 🚀

--- _downloads/7e9f897207d30ce909510639a8680ebc/surface-boolean.py
+++ _downloads/7e9f897207d30ce909510639a8680ebc/surface-boolean.py
@@ -14,20 +14,20 @@ from pyvista import _vtk as vtk
 ###############################################################################
 # Make a gridded volume
 n = 51
-xx = yy = zz = 1 - np.linspace(0, n, n) * 2 / (n-1)
+xx = yy = zz = 1 - np.linspace(0, n, n) * 2 / (n - 1)
 dataset = pv.RectilinearGrid(xx, yy, zz)
 
 ###############################################################################
 # Define a surface within the volume
-surface = pv.Cone(direction=(0,0,-1), height=3.0, radius=1,
-                  resolution=50, capping=False)
+surface = pv.Cone(
+    direction=(0, 0, -1), height=3.0, radius=1, resolution=50, capping=False
+)
 
 ###############################################################################
 # Preview the problem
 p = pv.Plotter()
-p.add_mesh(surface, color='w', label='Surface')
-p.add_mesh(dataset, color='gold', show_edges=True,
-           opacity=0.75, label='To Clip')
+p.add_mesh(surface, color="w", label="Surface")
+p.add_mesh(dataset, color="gold", show_edges=True, opacity=0.75, label="To Clip")
 p.add_legend()
 p.show()
 
@@ -42,7 +42,7 @@ dataset.compute_implicit_distance(surface, inplace=True)
 # Take note of the new ``implicit_distance`` scalar array. We will use this
 # to fill in regions inside the surface with the value 3.0 and regions outside
 # the surface with the value 2.0
-dataset['my_array'] = np.zeros(dataset.n_points)
-dataset['my_array'][dataset['implicit_distance'] >= 0] = 2.0
-dataset['my_array'][dataset['implicit_distance'] < 0] = 3.0
-dataset.plot(scalars='my_array', n_colors=2, clim=[1.5, 3.5])
+dataset["my_array"] = np.zeros(dataset.n_points)
+dataset["my_array"][dataset["implicit_distance"] >= 0] = 2.0
+dataset["my_array"][dataset["implicit_distance"] < 0] = 3.0
+dataset.plot(scalars="my_array", n_colors=2, clim=[1.5, 3.5])
